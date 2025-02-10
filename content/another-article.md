b1fbb08e25962f877a76e19067213970d3ebb967
Leniel Luzardo <iam@lenielluzardo.com>
2025-02-10 09:56:41 -0300

    version 0.1

--- content/another-article.md
+++ content/another-article.md
@@ -1,10 +1,7 @@
-# This is another article
-```
-Maybe this will apear as code
-```
-This is a paragraph 
-more content here...
-
-- One
-- two
-- three
+---
+title: My Article Title
+date: 2025-02-10
+---
+This is an example article with front matter and templating using Handlebars.
+![Example Image](http://localhost:1337/uploads/example_image.jpg)
+More content goes here...
--- generate.js
+++ generate.js
@@ -1,19 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 const marked = require('marked');
+const matter = require('gray-matter');
+const Handlebars = require('handlebars');
 
 const inputDir = path.join(__dirname, 'content');
 const outputDir = path.join(__dirname, 'public/blog/');
+const templatePath = path.join(__dirname, 'template.hbs');
 
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir);
 }
+const templateSource = fs.readFileSync(templatePath, 'utf-8');
+const template = Handlebars.compile(templateSource);
+
 const files = fs.readdirSync(inputDir).filter(file => file.endsWith('.md'));
 
 files.forEach(file => {
   const content = fs.readFileSync(path.join(inputDir, file), 'utf-8');
 
-  const html = marked.parse(content);
+  const { data, content: markdownContent } = matter(content);
+  
+  const htmlContent = marked.parse(markdownContent);
+
+  const html = template({
+    title: data.title,
+    header: "This is the header",
+    content: htmlContent,
+    date: data.date,
+    footer: "this is the footer",
+  });
 
   const outputFileName = file.replace('.md', '.html');
   fs.writeFileSync(path.join(outputDir, outputFileName), html);
