32860cdcc229622b3badba4977e338a53d83b367
Leniel Luzardo <lluzardo@galiciaseguros.com.ar>
2025-11-13 00:28:28 -0300

    adding logs to the netlify deploy script

--- www/build.sh
+++ www/build.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 set -e
 
-INIT_SUBMODULE="echo \"Git submodules: Initializing...\" && git submodule update --init --recursive"
-UPDATE_SUBMODULE="echo \"Git submodules: Fetching from remote...\" && git submodule update --remote"
-GIT_CHECKOUT="echo \"Git: checkout to blog branch...\" && git checkout -b blog"
-GIT_ADD="echo \"Git: Adding submod last pointer commit...\" && git add ./src/blog"
-GIT_COMMIT="echo \"Git: Commiting change...\" && git commit -m \"update submodule pointer commit\""
-GIT_PUSH="echo \"Git: Commiting change...\" && git push --set-upstream origin blog"
+LOG_INIT_SUBMODULE="echo \"Git submodules: Initializing...\""
+INIT_SUBMODULE="git submodule update --init --recursive"
+LOG_UPDATE_SUBMODULE="echo \"Git submodules: Fetching from remote...\""
+UPDATE_SUBMODULE="git submodule update --remote"
+LOG_GIT_CHECKOUT="echo \"Git: checkout to blog branch...\""
+GIT_CHECKOUT="git checkout -b blog"
+LOG_GIT_ADD="echo \"Git: Adding submod last pointer commit...\""
+GIT_ADD="git add ./src/blog"
+LOG_GIT_COMMIT="echo \"Git: Commiting change...\""
+GIT_COMMIT="git commit -m \"update submodule pointer commit\""
+LOG_GIT_PUSH="echo \"Git: Commiting change...\""
+GIT_PUSH="git push --set-upstream origin blog"
 BUILD_SITE="npm install && npx eleventy"
 
+$LOG_INIT_SUBMODULE
 $INIT_SUBMODULE
+$LOG_UPDATE_SUBMODULE
 $UPDATE_SUBMODULE
+$LOG_GIT_CHECKOUT
 $GIT_CHECKOUT
+$LOG_GIT_ADD
 $GIT_ADD
+$LOG_GIT_COMMIT
 $GIT_COMMIT
+$LOG_GIT_PUSH
 $GIT_PUSH
 $BUILD_SITE
