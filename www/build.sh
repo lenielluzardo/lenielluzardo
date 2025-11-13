b5095abc88e7da6a703a896be53c15a5a7ea2e38
Leniel Luzardo <lluzardo@galiciaseguros.com.ar>
2025-11-13 00:21:32 -0300

    adding logs to netlify deploy script

--- www/build.sh
+++ www/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-INIT_SUBMODULE="git submodule update --init --recursive"
-UPDATE_SUBMODULE="git submodule update --remote"
-GIT_CHECKOUT="git checkout -b blog"
-GIT_ADD="git add ./src/blog"
-GIT_COMMIT="git commit -m \"update submodule pointer commit\""
-GIT_PUSH="git push --set-upstream origin blog"
+INIT_SUBMODULE="echo \"Git submodules: Initializing...\" && git submodule update --init --recursive"
+UPDATE_SUBMODULE="echo \"Git submodules: Fetching from remote...\" && git submodule update --remote"
+GIT_CHECKOUT="echo \"Git: checkout to blog branch...\" && git checkout -b blog"
+GIT_ADD="echo \"Git: Adding submod last pointer commit...\" && git add ./src/blog"
+GIT_COMMIT="echo \"Git: Commiting change...\" && git commit -m \"update submodule pointer commit\""
+GIT_PUSH="echo \"Git: Commiting change...\" && git push --set-upstream origin blog"
 BUILD_SITE="npm install && npx eleventy"
 
 $INIT_SUBMODULE
