5e46f1f8dbddbc5cfa00c18cb8191733d7a96263
Leniel Luzardo <lluzardo@galiciaseguros.com.ar>
2025-11-14 00:01:20 -0300

    init styles

--- www/eleventy.config.js
+++ www/eleventy.config.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig)
 {
+    eleventyConfig.addPassthroughCopy("src/style.css");
+    eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/blog/assets");
 
     return {
